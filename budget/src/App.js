7c0a76f529af9d646be803f7ed066a5e718df286
Cristian <crifergon1@gmail.com>
2022-08-18 23:15:42 +0200

    start lv.1

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Check from './components/scene/form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <p>¿Qué quieres hacer?</p>
+      <form action="">
+        <div>
+        <Check service="web" value={500}></Check>
+        <label>Una pàgina web (500€)</label>
+        </div>
+        <div>
+        <Check service="seo" value={300}></Check>
+        <label>Una consultoria SEO (300€)</label>
+        </div>
+        <div>
+        <Check service="adds" value={200}></Check>
+        <label>Una campaña de Google Ads (200€)</label>
+        </div>
+      </form>
+    </>
   );
 }
 
---
+++ budget/src/components/scene/form.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Check = props => <input type="checkbox" name="budget" service={props.serv} value={props.value}></input>
+
+export default Check;
